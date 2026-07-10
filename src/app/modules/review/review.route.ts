0a1317dc92223f39ab27f05c9922cdfa357d2523
khaled8787 <khaledmahmud01737@gmail.com>
2026-07-10 20:06:07 +0600

    complete review module routing setup

--- src/app/modules/review/review.route.ts
+++ src/app/modules/review/review.route.ts
@@ -1,34 +1,34 @@
-// import { Router } from "express";
-// import { Role } from "@prisma/client";
-// import auth from "../../middlewares/auth";
-// import validateRequest from "../../middlewares/ValidateRequest";
-// import { ReviewController } from "./review.controller";
-// import { ReviewValidation } from "./review.validation";
+import { Router } from "express";
+import { Role } from "@prisma/client";
+import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/ValidateRequest";
+import { ReviewController } from "./review.controller";
+import { ReviewValidation } from "./review.validation";
 
-// const router = Router();
+const router = Router();
 
-// router.post(
-//   "/",
-//   auth(Role.CUSTOMER),
-//   validateRequest(ReviewValidation.createReviewValidationSchema),
-//   ReviewController.createReview
-// );
+router.post(
+  "/",
+  auth(Role.CUSTOMER),
+  validateRequest(ReviewValidation.createReviewValidationSchema),
+  ReviewController.createReview
+);
 
-// router.get("/", ReviewController.getAllReviews);
+router.get("/", ReviewController.getAllReviews);
 
-// router.get("/:id", ReviewController.getSingleReview);
+router.get("/:id", ReviewController.getSingleReview);
 
-// router.patch(
-//   "/:id",
-//   auth(Role.CUSTOMER),
-//   validateRequest(ReviewValidation.updateReviewValidationSchema),
-//   ReviewController.updateReview
-// );
+router.patch(
+  "/:id",
+  auth(Role.CUSTOMER),
+  validateRequest(ReviewValidation.updateReviewValidationSchema),
+  ReviewController.updateReview
+);
 
-// router.delete(
-//   "/:id",
-//   auth(Role.CUSTOMER),
-//   ReviewController.deleteReview
-// );
+router.delete(
+  "/:id",
+  auth(Role.CUSTOMER),
+  ReviewController.deleteReview
+);
 
-// export const ReviewRoutes = router;
+export const ReviewRoutes = router;
