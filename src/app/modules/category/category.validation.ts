0438a4a553a9d12f6e703eeb7d3859e7c649601a
khaled8787 <khaledmahmud01737@gmail.com>
2026-09-12 17:25:20 +0600

    category validation updated

--- src/app/modules/category/category.validation.ts
+++ src/app/modules/category/category.validation.ts
@@ -6,37 +6,43 @@ const categoryFieldsSchema = {
     .trim()
     .min(2, "Category name must be at least 2 characters.")
     .max(50, "Category name must not exceed 50 characters."),
+
   description: z
     .string()
     .trim()
     .max(500, "Description must not exceed 500 characters.")
     .optional(),
+
   icon: z
     .url("Icon must be a valid URL.")
     .optional(),
 };
 
 const createCategoryValidationSchema = z.object({
-  name: categoryFieldsSchema.name,
-  description: categoryFieldsSchema.description,
-  icon: categoryFieldsSchema.icon,
-});
-
-const updateCategoryValidationSchema = z
-  .object({
-    name: categoryFieldsSchema.name.optional(),
+  body: z.object({
+    name: categoryFieldsSchema.name,
     description: categoryFieldsSchema.description,
     icon: categoryFieldsSchema.icon,
-  })
-  .refine(
-    (data) =>
-      data.name !== undefined ||
-      data.description !== undefined ||
-      data.icon !== undefined,
-    {
-      message: "At least one field must be provided for update.",
-    }
-  );
+  }),
+});
+
+const updateCategoryValidationSchema = z.object({
+  body: z
+    .object({
+      name: categoryFieldsSchema.name.optional(),
+      description: categoryFieldsSchema.description,
+      icon: categoryFieldsSchema.icon,
+    })
+    .refine(
+      (data) =>
+        data.name !== undefined ||
+        data.description !== undefined ||
+        data.icon !== undefined,
+      {
+        message: "At least one field must be provided for update.",
+      }
+    ),
+});
 
 export const CategoryValidation = {
   createCategoryValidationSchema,
